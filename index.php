700d0d0acf698caa8ac7b29f09957919da2abe0f
clownfenner <raphaelfenner@gmail.com>
2020-06-08 03:05:37 -0300

    Pagina inicial

--- index.php
+++ index.php
@@ -8,10 +8,100 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Kachaw - Inicio</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <div class="container">
+    <div id="topo">
+        <div class="text-center">
+            <img src="imagens/logo.png">
+        </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(atual)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Minha Conta</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Carrinho</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categorias
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sobre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Suporte</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Digite o nome..." aria-label="Pesquisar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+  </div>
+</nav> 
+    </div>
+
+    <div id="meio" class="row">
+    <div class="col-4 my-5">
+      <img src="http://placehold.it/250x320" class="d-block mx-auto">
+      <p class="text-center">Nome do produto:</p>
+      <p class="text-center">Preço:</p>
+      <button type="button" class="btn btn-outline-success d-block mx-auto">Comprar</button>
+    </div>
+    <div class="col-4 my-5">
+      <img src="http://placehold.it/250x320" class="d-block mx-auto">
+      <p class="text-center">Nome do produto:</p>
+      <p class="text-center">Preço:</p>
+      <button type="button" class="btn btn-outline-success d-block mx-auto">Comprar</button>
+    </div>
+    <div class="col-4 my-5">
+      <img src="http://placehold.it/250x320" class="d-block mx-auto">
+      <p class="text-center">Nome do produto:</p>
+      <p class="text-center">Preço:</p>
+      <button type="button" class="btn btn-outline-success d-block mx-auto">Comprar</button>
+    </div>
+    <div class="col-4 my-5">
+      <img src="http://placehold.it/250x320" class="d-block mx-auto">
+      <p class="text-center">Nome do produto:</p>
+      <p class="text-center">Preço:</p>
+      <button type="button" class="btn btn-outline-success d-block mx-auto">Comprar</button>
+    </div>
+    <div class="col-4 my-5">
+      <img src="http://placehold.it/250x320" class="d-block mx-auto">
+      <p class="text-center">Nome do produto:</p>
+      <p class="text-center">Preço:</p>
+      <button type="button" class="btn btn-outline-success d-block mx-auto">Comprar</button>
+    </div>
+    <div class="col-4 my-5">
+      <img src="http://placehold.it/250x320" class="d-block mx-auto">
+      <p class="text-center">Nome do produto:</p>
+      <p class="text-center">Preço:</p>
+      <button type="button" class="btn btn-outline-success d-block mx-auto">Comprar</button>
+    </div>
+
+    
+    
+
+
+    
+    </div>
+
+
+</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
